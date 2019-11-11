b0a1b0945023aefb53b1bd209e5f10ee63078cac
UnrealWind <fengyan19921992@qq.com>
2019-11-11 16:27:16 +0800

    add login page

---
+++ src/pages/user/accountlogin.vue
@@ -0,0 +1,172 @@
+<!-- home -->
+<template>
+  <van-container :status="status">
+    <div slot="header" class="fix">
+      <div class="wp">
+        <div class="header_l" @click="$router.back()">
+          <van-icon name="arrow-left" />
+        </div>
+      </div>
+    </div>
+
+    <div class="dan_wrap">
+      <div class="wp">
+
+        <div class="title"> 账号登录 </div>
+
+        <div class="account">
+          <div class="acc-Title">  账号 </div>
+          <van-cell-group>
+            <van-field v-model="valuemin" placeholder="请输入用户名" />
+          </van-cell-group>
+        </div>
+
+        <div class="account">
+          <div class="acc-Title"> 密码 </div>
+
+          <van-cell-group>
+            <van-field
+              v-model="password"
+              type="password"
+              placeholder="请输入密码"
+            />
+          </van-cell-group>
+
+        </div>
+
+        <div class="register" @click="$router.push('/')">
+          登录
+        </div>
+
+      </div>
+    </div>
+
+  </van-container>
+</template>
+
+<script>
+  import { Icon, Field, CellGroup } from 'vant'
+
+  export default {
+    components: {
+        'van-icon': Icon,
+        'van-field': Field,
+        'van-cell-group': CellGroup
+    },
+    data() {
+      return {
+          status: 'loading',
+          value: '',
+          valuemin: '',
+          showKeyboard: true
+      }
+    },
+    computed: {
+    },
+    mounted() {
+        this.init()
+    },
+    methods: {
+        async init() {
+            try {
+                // await this.getData()
+            } catch (e) {
+                this.status = 'error'
+                throw e
+            }
+            this.status = 'success'
+        },
+        async getData() {
+            const res = await this.$http.get('/user/12345')
+            console.log(res)
+        }
+    }
+  }
+
+</script>
+<style lang='scss' scoped>
+
+  h1 {
+    background: red;
+    width: 375px;
+  }
+  .fix {
+    *zoom: 1;
+  }
+  .l{ float: left; }
+  .r{ float: right; }
+  .fix:after,
+  .fix:before {
+    display: block;
+    content: "clear";
+    height: 0;
+    clear: both;
+    overflow: hidden;
+    visibility: hidden;
+  }
+  .main{
+    background: #ffffff;
+  }
+  .wp{
+    width: 95%;
+    margin: 0 auto;
+  }
+
+  .dan_wrap{
+    padding-top: 30%;
+    height: 100%;
+    background: #ffffff;
+    border-top: 1px solid #dddddd;
+
+    .title{
+      font-size: 20px;
+      padding-top: 30px;
+      line-height: 30px;
+      margin-bottom: 10px;
+      font-weight: bold;
+    }
+  }
+
+  .header{
+    .fix{
+      position: relative;
+      background: #ffffff;
+    }
+    .header_l{
+      position: absolute;
+      left: 0px;
+      top: 0px;
+      color: #212121;
+      font-size: 20px;
+    }
+    .header_title{
+      text-align: center;
+      margin-right: 10px;
+      color: #212121;
+    }
+
+  }
+
+  .account{
+    .acc-Title{
+      margin-top: 20px;
+      color: #ccc;
+      font-size: 16px;
+      margin-bottom: 10px;
+    }
+
+  }
+
+  .register{
+    margin-top: 10%;
+    width: 100%;
+    border-radius: 5px;
+    color: #fff;
+    background: #ccc;
+    font-size: 16px;
+    text-align: center;
+    line-height: 30px;
+    padding: 6px 0px;
+  }
+
+  </style>
