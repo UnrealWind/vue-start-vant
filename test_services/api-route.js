fb1d9d2874e60f889024d5ec62d53079c8c91d80
UnrealWind <fengyan19921992@qq.com>
2019-12-24 15:22:49 +0800

    增加了mockjs的说明

--- test_services/api-route.js
+++ test_services/api-route.js
@@ -1,32 +1,66 @@
 /**
  * API 接口路由全部在这里定义
+ * mockjs 参考 http://mockjs.com/examples.html
  */
 
 const express = require('express')
 const mockjs = require('mockjs')
 
-var router = express.Router()
+const router = express.Router()
 
-router.get('/wechat/oauth2/get/user', function(req, res, next) {
-  var data = mockjs.mock({
+router.post('/test/:id', function(req, res, next) {
+  const paramsId = req.params.id
+  const queryId = req.query.id
+  console.log(paramsId, queryId)
+  const data = mockjs.mock({
     'data': {
-      'id': 7,
-      'subscribe': null,
-      'openId': 'oXL5M1pApcenxiH9gluvrjTTTB2A',
-      'nickname': 'Schrodinger',
-      'sex': 1,
-      'language': 'zh_CN',
-      'city': '石家庄',
-      'province': '河北',
-      'country': '中国',
-      'headImgUrl': 'http://thirdwx.qlogo.cn/mmopen/vi_32/icibHCyZDte5IY7HEic2q0N6HQRh5kVJtXkjYtYOn7gmiaD12ZEIObBbkO82hhHLR4Iq889oiajGHAD07raM3xJfxRQ/132',
-      'subscribeTime': null,
-      'unionId': null,
-      'remark': null,
-      'groupId': null,
-      'subscribeScene': null,
-      'qrScene': null,
-      'qrSceneStr': null
+      'id': '@id',
+      'nickname': '@cname()',
+      'sex|1': [1, 2],
+      'time': "@datetime('yyyy-MM-dd HH:mm:ss')",
+      'total|10000-99999': 0,
+      'name': '@ctitle(1,20)模板',
+      'subjectPlans|3': [
+        {
+          'id': '@id',
+          'name': '@word(3, 5)',
+          'time': "@date('yyyy-MM-dd')",
+          'type|1': ['baseInterview', 'interview', 'tempInterview', 'endingInterview', 'resultInterview',
+            'questionInterview', 'scoreInterview'],
+          'crfTemplateId': '@id',
+          'crfTemplateName': '@cword(5,10)-CRF'
+        }
+      ]
+    },
+    'status': 'ok',
+    'description': '数据请求成功'
+  })
+  res.json(data)
+})
+
+router.get('/test/:id', function(req, res, next) {
+  const paramsId = req.params.id
+  const queryId = req.query.id
+  console.log(paramsId, queryId)
+  const data = mockjs.mock({
+    'data': {
+      'id': '@id',
+      'nickname': '@cname()',
+      'sex|1': [1, 2],
+      'time': "@datetime('yyyy-MM-dd HH:mm:ss')",
+      'total|10000-99999': 0,
+      'name': '@ctitle(1,20)模板',
+      'subjectPlans|3': [
+        {
+          'id': '@id',
+          'name': '@word(3, 5)',
+          'time': "@date('yyyy-MM-dd')",
+          'type|1': ['baseInterview', 'interview', 'tempInterview', 'endingInterview', 'resultInterview',
+            'questionInterview', 'scoreInterview'],
+          'crfTemplateId': '@id',
+          'crfTemplateName': '@cword(5,10)-CRF'
+        }
+      ]
     },
     'status': 'ok',
     'description': '数据请求成功'
