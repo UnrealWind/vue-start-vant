313f15a25432a76f5a171828810af208bd196966
UnrealWind <fengyan19921992@qq.com>
2019-12-26 09:54:04 +0800

    add share button and swipper link

--- src/components/socket.js
+++ src/components/socket.js
@@ -6,7 +6,7 @@ const init = {
     console.log(store)
 
     // fydebug整了半天，vue进行全局组件注册的时候实在是没办法带上store的参数，目前先改成这个样子，后台进行修改后这边会对应进行调整
-    const wsurl = `ws://${config.socketUrl}/imServer.do?userCode=u002&userType=0&osType=0`
+    const wsurl = `wss://${config.socketUrl}/imServer.do?userCode=u002&userType=0&osType=0`
     const socketServer = new WebSocket(wsurl)
     socketServer['sendMsg'] = (data) => {
       socketServer.send(JSON.stringify(data))
--- src/config/config.js
+++ src/config/config.js
@@ -32,9 +32,9 @@ const config = {
     '/example'
   ],
   backStageUrl: process.env.VUE_APP_BASE_URL,
-  socketUrl: 'http://115.29.109.95:80/socket/', // 链接websocket
+  socketUrl: 'www.jipaisc.com/socket', // 链接websocket
   // socketUrl: '127.0.0.1:8088/', // 链接websocket
-  fileUploadUrl: 'http://115.29.109.95:80/resource-manager/file/', // 上传文件
+  fileUploadUrl: 'https://www.jipaisc.com/resource-manager/file/', // 上传文件
   // fileUploadUrl: 'http://127.0.0.1:8080/', // 上传文件
   mpUrl: 'https://www.zufangt.com', // 使用微信接口的url
   jssdkUrl: process.env.VUE_APP_BASE_URL + '/wx/getSign' // 后台换权限的接口
